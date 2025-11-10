ba9e888f66cc1ebefcec74a6ccb0ff1ed991fa45
macacolabs <monkey.ohgiraffers@gmail.com>
2025-11-10 09:32:44 +0900

    commit

--- chap02-algorithms/src/main/java/com/ohgiraffers/section02/searching/C_ShortestPath.java
+++ chap02-algorithms/src/main/java/com/ohgiraffers/section02/searching/C_ShortestPath.java
@@ -1,4 +1,12 @@
 package com.ohgiraffers.section02.searching;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
 /*
 * BFS는 먼저 방문한 노드를 기준으로 가장 가까운 노드부터 탐색한다.
 * 즉, 시작점에서 출발하여 거리 1짜리 노드를 모두 방문한 후,
@@ -24,4 +32,30 @@ public class C_ShortestPath {
             this.y = y;
         }
     }
+
+    static Queue<Node> q = new LinkedList<>();
+
+    public static int solution(String input) throws IOException {
+
+        BufferedReader br = new BufferedReader(new StringReader(input));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+        visit = new boolean[N][M];
+
+        for(int i = 0; i < N; i++){
+            char[] ch = br.readLine().toCharArray();
+            for(int j = 0; j < ch.length; j++){
+                map[i][j] = Character.getNumericValue(ch[j]);
+            }
+        }
+
+        /* 0, 0 에서 출발 */
+        bfs(0, 0);
+
+        /* 도착지의 값을 반환 (도착 할 때까지의 최적의 경로 단계 수) */
+        return map[N-1][M-1];
+    }
+
 }
